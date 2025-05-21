ac043ddcb167231f29d970e3278a3bd36674486b
myleftshoe <dev.paulesammut@gmail.com>
2025-05-21 20:40:16 +1000

    .

--- src/routes/examples/floater2.svelte
+++ src/routes/examples/floater2.svelte
@@ -3,7 +3,7 @@
 	import { X } from '@lucide/svelte'
 	let open = $state(false)
 	let justify = $state('center')
-	let resistance = $state('normal')
+	let type = $state('')
 </script>
 
 <div class="space-y-2">
@@ -13,17 +13,17 @@
 		<option value="start">Start</option>
 		<option value="end">End</option>
 	</select>
-	<select class="select" bind:value={resistance}>
-		<option value="none">None</option>
-		<option value="normal">Normal</option>
-		<option value="futile">Futile</option>
+	<select class="select" bind:value={type}>
+		<option value="">Default</option>
+		<option value="dismissible">Dismissible</option>
+		<option value="fixed">Fixed</option>
 	</select>
 	<button class="btn btn-sm preset-outlined-secondary-600-400" onclick={() => (open = true)}>Open</button>
 </div>
 
 <link rel="preload" as="image" href="https://picsum.photos/id/830/170/170" />
 
-<Floatingsheet bind:open {justify} {resistance}>
+<Floatingsheet bind:open {justify} {type}>
 	<button class="absolute top-4 right-4 btn-icon preset-filled-surface-500 rounded-full" onclick={() => (open = false)}><X /></button>
 	<div class="text-center flex flex-col items-center pt-6 pb-6 px-4 gap-4 text-balance">
 		<img class="rounded-2xl h-[170px]" src="https://picsum.photos/id/830/170/170" alt="" />
